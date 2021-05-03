d27f675770197160927e946ab92dbe8a76cb5c92
Tri Phan <phanphutri@gmail.com>
2021-05-03 18:59:58 +0700

    Added 01_Introduction

---
+++ 01_Introduction/script.js
@@ -0,0 +1,14 @@
+let data;
+console.log(typeof data); // undefined
+
+data = 5;
+console.log(typeof data); // number
+
+data = 'hello';
+console.log(typeof data); // string
+
+data = true;
+console.log(typeof data); // boolean
+
+data = {};
+console.log(typeof data); // object
